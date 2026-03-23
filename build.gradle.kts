4fcf72bb90fa5be3d7fa4563fe3ad9085d3f81ab
Bed <bahaeds.2023@gmail.com>
2026-03-23 01:29:50 +0100

    added lamp

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     testImplementation("org.tinylog:tinylog-api-kotlin:2.8.0-M1")
     testImplementation("org.tinylog:tinylog-impl:2.8.0-M1")
     testImplementation("org.tinylog:slf4j-tinylog:2.8.0-M1")
+    testImplementation("io.github.revxrsal:lamp.common:4.0.0-rc.16")
+    testImplementation("io.github.revxrsal:lamp.minestom:4.0.0-rc.16")
 }
 
 kotlin {
@@ -27,9 +29,16 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>("compileTestKotlin") {
+    compilerOptions {
+        javaParameters.set(true)
+    }
+}
+
 tasks.register<JavaExec>("runTestServer") {
     group = "application"
     description = "Runs test server"
+
     dependsOn(tasks.named("testClasses"))
 
     classpath = sourceSets["test"].runtimeClasspath
