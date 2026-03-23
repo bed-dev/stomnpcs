cdaec3bb03992ac8e6249ef29352ebbac5c4200e
Bed <bahaeds.2023@gmail.com>
2026-03-23 04:00:57 +0100

    refactored NPC implementation with improved metadata handling, added interaction and attack events, and enhanced text display synchronization

--- src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
+++ src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
@@ -5,86 +5,156 @@ import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.coordinate.Vec
 import net.minestom.server.entity.*
-import net.minestom.server.entity.metadata.display.AbstractDisplayMeta
+import net.minestom.server.entity.metadata.avatar.PlayerMeta
 import net.minestom.server.entity.metadata.display.TextDisplayMeta
 import net.minestom.server.entity.metadata.other.InteractionMeta
+import net.minestom.server.event.entity.EntityAttackEvent
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerEntityInteractEvent
+import net.minestom.server.event.player.PlayerPacketOutEvent
 import net.minestom.server.instance.Instance
 import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
+import net.minestom.server.network.packet.server.play.PlayerInfoRemovePacket
 import net.minestom.server.network.packet.server.play.PlayerInfoUpdatePacket
 import net.minestom.server.utils.chunk.ChunkSupplier
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.minestom.MinestomLamp
-
+import java.util.*
 
 fun main() {
-    // Initialization
     val minecraftServer = MinecraftServer.init()
-
-    // Create the instance
     val instanceManager = MinecraftServer.getInstanceManager()
     val instanceContainer = instanceManager.createInstanceContainer()
 
-    // Set the ChunkGenerator
     instanceContainer.setGenerator { unit ->
         unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
     }
     instanceContainer.chunkSupplier = ChunkSupplier { instance, x, z -> LightingChunk(instance, x, z) }
 
-    // Add an event callback to specify the spawning instance (and the spawn position)
     val globalEventHandler = MinecraftServer.getGlobalEventHandler()
+
     globalEventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
         event.spawningInstance = instanceContainer
         event.player.respawnPoint = Pos(0.0, 42.0, 0.0)
     }
 
-    minecraftServer.start("0.0.0.0", 25565)
+    globalEventHandler.addListener(PlayerPacketOutEvent::class.java) { event ->
+        println("[PACKET OUT] -> ${event.packet.javaClass.simpleName} to ${event.player.username}")
+    }
 
+    MinecraftServer.getExceptionManager().setExceptionHandler { e ->
+        println("[MINESTOM EXCEPTION] Caught an internal error:")
+        e.printStackTrace()
+    }
+
+    minecraftServer.start("0.0.0.0", 25565)
 
     val lamp = MinestomLamp.builder().build()
     lamp.register(TestCommands())
 }
 
-fun spawnTestNpc(instance: Instance, pos: Pos, skin: PlayerSkin) {
-    val npc = object : Entity(EntityType.PLAYER) {
+fun spawnTestNpc(instance: Instance, position: Pos, skin: PlayerSkin, name: String): Entity {
+    val nameTagOffset = Vec(0.0, 2.15, 0.0)
+    val hitboxOffset = Vec(0.0, 0.9, 0.0)
+    val profileName = name.filter { it.isLetterOrDigit() || it == '_' }.ifBlank { "npc" }.take(16)
+    val npcUuid = UUID.randomUUID()
+
+    lateinit var nameTag: Entity
+    lateinit var hitbox: Entity
+
+    val npc = object : Entity(EntityType.PLAYER, npcUuid) {
+        override fun movementTick() {}
+
         override fun updateNewViewer(player: Player) {
-            val entry = PlayerInfoUpdatePacket.Entry(
-                uuid, "NPC",
-                listOf(PlayerInfoUpdatePacket.Property("textures", skin.textures(), skin.signature())),
-                false, 0, GameMode.SURVIVAL, null, null, 0, false
+            val properties = listOf(
+                PlayerInfoUpdatePacket.Property("textures", skin.textures(), skin.signature())
             )
+            val entry = PlayerInfoUpdatePacket.Entry(
+                uuid,
+                profileName,
+                properties,
+                false,
+                0,
+                GameMode.SURVIVAL,
+                null,
+                null,
+                0,
+                true
+            )
+
             player.sendPacket(PlayerInfoUpdatePacket(PlayerInfoUpdatePacket.Action.ADD_PLAYER, entry))
             super.updateNewViewer(player)
         }
-    }
 
-    val nameTag = Entity(EntityType.TEXT_DISPLAY)
-    nameTag.editEntityMeta(TextDisplayMeta::class.java) { meta ->
-        meta.text = Component.text("TEXT DISPLAY")
-        meta.billboardRenderConstraints = AbstractDisplayMeta.BillboardConstraints.CENTER
-        meta.backgroundColor = 0
-        meta.translation = Vec(0.0, 2.3, 0.0)
-    }
+        override fun updateOldViewer(player: Player) {
+            super.updateOldViewer(player)
+            player.sendPacket(PlayerInfoRemovePacket(uuid))
+        }
 
+        override fun update(time: Long) {
+            super.update(time)
+            if (nameTag.instance != null) nameTag.teleport(this.position.add(nameTagOffset))
+            if (hitbox.instance != null) hitbox.teleport(this.position.add(hitboxOffset))
+        }
 
-    val hitbox = Entity(EntityType.INTERACTION)
-    hitbox.editEntityMeta(InteractionMeta::class.java) { meta ->
-        meta.width = 0.6f
-        meta.height = 1.8f
-    }
-
-    instance.eventNode().addListener(PlayerEntityInteractEvent::class.java) { event ->
-        if (event.target == hitbox) {
-            event.player.sendMessage(Component.text("You interacted with the NPC!"))
+        override fun remove() {
+            if (nameTag.instance != null) nameTag.remove()
+            if (hitbox.instance != null) hitbox.remove()
+            super.remove()
         }
     }
 
-    npc.setInstance(instance, pos).thenRun {
-        nameTag.setInstance(instance, pos).thenRun { npc.addPassenger(nameTag) }
-        hitbox.setInstance(instance, pos).thenRun { npc.addPassenger(hitbox) }
+    npc.setNoGravity(false)
+    npc.setCustomName(Component.text(name))
+    npc.isCustomNameVisible = true
+
+    npc.editEntityMeta(PlayerMeta::class.java) { meta ->
+        meta.isCapeEnabled = true
+        meta.isJacketEnabled = true
+        meta.isLeftSleeveEnabled = true
+        meta.isRightSleeveEnabled = true
+        meta.isLeftLegEnabled = true
+        meta.isRightLegEnabled = true
+        meta.isHatEnabled = true
     }
+
+    nameTag = Entity(EntityType.TEXT_DISPLAY).apply {
+        setNoGravity(true)
+        editEntityMeta(TextDisplayMeta::class.java) { meta ->
+            meta.text = Component.text(name)
+        }
+    }
+
+    hitbox = Entity(EntityType.INTERACTION).apply {
+        setNoGravity(true)
+        editEntityMeta(InteractionMeta::class.java) { meta ->
+            meta.width = 0.75f
+            meta.height = 1.95f
+            meta.response = true
+        }
+    }
+
+    npc.setInstance(instance, position)
+    nameTag.setInstance(instance, position.add(nameTagOffset))
+    hitbox.setInstance(instance, position.add(hitboxOffset))
+
+    val global = MinecraftServer.getGlobalEventHandler()
+
+    global.addListener(PlayerEntityInteractEvent::class.java) { event ->
+        if (event.target.uuid == hitbox.uuid || event.target.uuid == npc.uuid) {
+            event.player.sendMessage(Component.text("You interacted with $name"))
+        }
+    }
+
+    global.addListener(EntityAttackEvent::class.java) { event ->
+        val player = event.entity as? Player ?: return@addListener
+        if (event.target.uuid == hitbox.uuid || event.target.uuid == npc.uuid) {
+            player.sendMessage(Component.text("You attacked $name"))
+        }
+    }
+
+    return npc
 }
 
 fun getSkin(username: String): PlayerSkin? {
@@ -95,23 +165,21 @@ class TestCommands {
 
     @Command("npc")
     fun npc(actor: Player) {
-        spawnTestNpc(actor.instance!!, actor.position, getSkin("Notch")!!)
+        spawnTestNpc(actor.instance, actor.position, getSkin("Notch")!!, "Notch")
     }
 
     @Command("gmc")
     fun gmc(actor: Player) {
-        actor.gameMode = net.minestom.server.entity.GameMode.CREATIVE
+        actor.gameMode = GameMode.CREATIVE
     }
 
     @Command("gms")
     fun gmsp(actor: Player) {
-        actor.gameMode = net.minestom.server.entity.GameMode.SURVIVAL
+        actor.gameMode = GameMode.SURVIVAL
     }
 
     @Command("stop")
     fun stop(actor: Player) {
         MinecraftServer.stopCleanly()
     }
-
-
 }
