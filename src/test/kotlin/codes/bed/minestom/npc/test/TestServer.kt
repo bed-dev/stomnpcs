dc42203d3faf5382b61ed636d116820a169b12e1
Bed <bahaeds.2023@gmail.com>
2026-03-23 01:17:05 +0100

    added tinylog properties and demo server

--- src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
+++ src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
@@ -1,2 +1,31 @@
 package codes.bed.minestom.npc.test
 
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.instance.block.Block
+
+
+fun main() {
+    // Initialization
+    val minecraftServer = MinecraftServer.init()
+
+    // Create the instance
+    val instanceManager = MinecraftServer.getInstanceManager()
+    val instanceContainer = instanceManager.createInstanceContainer()
+
+    // Set the ChunkGenerator
+    instanceContainer.setGenerator { unit ->
+        unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
+    }
+
+    // Add an event callback to specify the spawning instance (and the spawn position)
+    val globalEventHandler = MinecraftServer.getGlobalEventHandler()
+    globalEventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
+        event.spawningInstance = instanceContainer
+        event.player.respawnPoint = Pos(0.0, 42.0, 0.0)
+    }
+
+    minecraftServer.start("0.0.0.0", 25565)
+
+}
