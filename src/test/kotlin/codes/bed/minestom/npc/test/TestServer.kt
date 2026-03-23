aef5037f7d7f43a6cfc31216eabf346debefe2f4
Bed <bahaeds.2023@gmail.com>
2026-03-23 01:34:12 +0100

    added test commands

--- src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
+++ src/test/kotlin/codes/bed/minestom/npc/test/TestServer.kt
@@ -2,8 +2,13 @@ package codes.bed.minestom.npc.test
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.Player
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
+import net.minestom.server.utils.chunk.ChunkSupplier
+import revxrsal.commands.annotation.Command
+import revxrsal.commands.minestom.MinestomLamp
 
 
 fun main() {
@@ -18,6 +23,7 @@ fun main() {
     instanceContainer.setGenerator { unit ->
         unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
     }
+    instanceContainer.chunkSupplier = ChunkSupplier { instance, x, z -> LightingChunk(instance, x, z) }
 
     // Add an event callback to specify the spawning instance (and the spawn position)
     val globalEventHandler = MinecraftServer.getGlobalEventHandler()
@@ -27,5 +33,33 @@ fun main() {
     }
 
     minecraftServer.start("0.0.0.0", 25565)
+
+
+    val lamp = MinestomLamp.builder().build()
+    lamp.register(TestCommands())
+}
+
+
+class TestCommands {
+
+    @Command("npc")
+    fun npc(actor: Player) {
+    }
+
+    @Command("gmc")
+    fun gmc(actor: Player) {
+        actor.gameMode = net.minestom.server.entity.GameMode.CREATIVE
+    }
+
+    @Command("gms")
+    fun gmsp(actor: Player) {
+        actor.gameMode = net.minestom.server.entity.GameMode.SURVIVAL
+    }
+
+    @Command("stop")
+    fun stop(actor: Player) {
+        MinecraftServer.stopCleanly()
+    }
+
 
 }
